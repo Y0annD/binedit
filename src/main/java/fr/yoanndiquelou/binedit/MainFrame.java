8f916383da7c3c2b6c577655a9477c249c51b4c0
Yoann Diquélou <yoann.diquelou@gmail.com>
2019-10-27 20:46:07 +0100

    BIN-8: Drag&Drop de fichiers

--- src/main/java/fr/yoanndiquelou/binedit/MainFrame.java
+++ src/main/java/fr/yoanndiquelou/binedit/MainFrame.java
@@ -1,8 +1,12 @@
 package fr.yoanndiquelou.binedit;
 
 import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.dnd.DnDConstants;
+import java.awt.dnd.DropTarget;
+import java.awt.dnd.DropTargetDropEvent;
+import java.io.File;
+import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -10,7 +14,6 @@ import javax.swing.JSplitPane;
 
 import fr.yoanndiquelou.binedit.menu.FrameMenu;
 import fr.yoanndiquelou.binedit.panel.ExplorerPanel;
-import fr.yoanndiquelou.binedit.panel.MDIPanel;
 
 public class MainFrame extends JFrame {
 
@@ -27,17 +30,40 @@ public class MainFrame extends JFrame {
 		setLocationRelativeTo(null);
 		setJMenuBar(new FrameMenu());
 		JPanel explorerPanel = new ExplorerPanel();
-		
+
 		JSplitPane pane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, explorerPanel, controller.getMDIPanel());
 		pane.setOneTouchExpandable(true);
 		pane.setDividerLocation(150);
-		//Provide minimum sizes for the two components in the split pane
+		// Provide minimum sizes for the two components in the split pane
 		Dimension minimumSize = new Dimension(100, 50);
 		explorerPanel.setMinimumSize(minimumSize);
 		controller.getMDIPanel().setMinimumSize(minimumSize);
+		controller.getMDIPanel().setDropTarget(new DropTarget() {
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = -6418118605479053389L;
+
+			@SuppressWarnings("unchecked")
+			public synchronized void drop(DropTargetDropEvent evt) {
+				try {
+					evt.acceptDrop(DnDConstants.ACTION_COPY);
+					List<File> droppedFiles = (List) evt.getTransferable()
+							.getTransferData(DataFlavor.javaFileListFlavor);
+					for (File file : droppedFiles) {
+						if (file.isFile()) {
+							controller.getInstance().openFile(file);
+						}
+					}
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		});
 		add(pane);
-//		pack();
+		pack();
 		setVisible(true);
 	}
 
+	
 }
